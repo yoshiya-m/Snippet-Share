7ea5aafdf4ebed7d41bbbc9d7855bb74a59ba027
yoshiya-m <2101115016rz@cyber-u.ac.jp>
2024-10-01 12:14:10 +0900

    add routes.php

--- Routing/routes.php
+++ Routing/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 use Helpers\DatabaseHelper;
 use Helpers\ValidationHelper;
 use Response\HTTPRenderer;
@@ -7,52 +6,21 @@ use Response\Render\HTMLRenderer;
 use Response\Render\JSONRenderer;
 
 return [
-    'random/part'=>function(): HTTPRenderer {
-        $part = DatabaseHelper::getRandomComputerPart();
+    "create" => function(): HTTPRenderer {
+    // 返すデータをどこで作成する? component/create.phpで作成？
+    // 返したいのはJSONデータで成功、失敗とURLを返したい
+    // しかし、HTMLrenはhtmlを返す
+    // JSOnはjsonデータだがほかのエンドポイントはデータ直渡し
+    // database helperに渡して結果を返す
 
-        return new HTMLRenderer('component/random-part', ['part' => $part]);
-    },
-    'parts' => function(): HTTPRenderer {
-        $id = ValidationHelper::integer($_GET['id']??null);
+        // データの受け取り
+        $inputText = $_POST['inputText'];
+        $expirationDate = $_POST['expirationDate'];
+        // データの検証
+        ValidationHelper::string($inputText);
+        // ここでjsonデータ作ってjsonrenderに投げる
+        $url = DatabaseHelper::createSnippet($inputText, $expirationDate);
 
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new HTMLRenderer('component/parts', ['part'=>$part]);
-    },
-    'api/random/part' => function(): HTTPRenderer {
-        $part = DatabaseHelper::getRandomComputerPart();
-        return new JSONRenderer(['part' => $part]);
-    },
-    'api/parts' => function() {
-        $id = ValidationHelper::integer($_GET['id']??null);
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new JSONRenderer(['part' => $part]);
-    },
-    'types' => function() {
-        $type = ValidationHelper::string($_GET['type']??null);
-        $page = ValidationHelper::integer($_GET['page']??null);
-        $perpage = ValidationHelper::integer($_GET['perpage']??null);
-
-        $part = DatabaseHelper::getComputerPartByType($type, $page, $perpage);
-        return new JSONRenderer(['part' => $part]);
-    },
-
-    'random/computer' => function() {
-        // コンピュータ生成は何の部品で構成？-> ランダムに4部品集めて返す
-        $computer = DatabaseHelper::getRandomComputer();
-        return new JSONRenderer(['computer' => $computer]);
-    },
-    'parts/newest' => function() {
-        $page = ValidationHelper::integer($_GET['page']??null);
-        $perpage = ValidationHelper::integer($_GET['perpage']??null);
-
-        $part = DatabaseHelper::getNewestComputerPart($page, $perpage);
-        return new JSONRenderer(['part' => $part]);
-    },
-    'parts/performance' => function(){
-        $order = ValidationHelper::string($_GET['order']??null);
-        $type = ValidationHelper::string($_GET['type']??null);
-        
-        $part = DatabaseHelper::getComputerPartByPerformance($order, $type);
-        return new JSONRenderer(['part' => $part]);
-    }   
+        return new JSONRenderer(["url" => $url]);
+    }
 ];
